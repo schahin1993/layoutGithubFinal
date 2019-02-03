710edc46febaf6a67c3a9918e6c1ee6ec4b4dccf
Mac_far <schahii_rez93@yahoo.com>
2019-02-03 10:09:53 +0330

    Initial Commit

---
+++ layoutG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layoutG
+//
+//  Created by Mac_far on 2/3/19.
+//  Copyright © 2019 Mac_far. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
